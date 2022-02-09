779ee4729c4e1d5cfdd40b63dca42ad11f639de7
Alex Morais <alex.morais@objectrocket.com>
2022-02-09 13:16:15 -0700

    initial commit + future garbage

--- README.md
+++ README.md
@@ -1,24 +1,64 @@
 # twitch-creamery
 a twitch-bot for the homies and I to entertain and amuse
 
-## Setup
+## setup
 
 1. Make an account on Twitch for your bot. Its more common than using your actual account.
-1. If you prefer, request an oauth code from: https://twitchapps.com/tmi using your bot account. You'll need to login and give the app permissions to generate it for you. You can also choose your own way of getting a token.
-1. Register your app with Twitch dev, https://dev.twitch.tv/console/apps/create , and request a client-id so you can interface with Twitch's API
+1. If you prefer, request an oauth code from: https://twitchtokengenerator.com/ using your bot account. You'll need to login and give the app permissions to generate it for you. This app will give you the access token and the client ID of your bot's account. You can also choose your own way of getting these credentials if you wish!
+1. Register an app with Twitch dev under your bot account, https://dev.twitch.tv/console/apps/create , and request a client-id so you can interface with Twitch's API. This Client ID is different from the one collected in the previous step; it is specifically for the API only. 
 
-You'll need these later.
-
+TODO: Figure out if we need a different Twitch API client ID
 TODO: Build the token generation into this app so that giving permissions to a 3rd party application is not necessary.
 
-### Install
+## env file
 
-1. Python 3.6 is sufficient, but Python 3.9 is the latest and is preferred:
-Windows: https://www.python.org/downloads/release/python-375/
-Linux: https://www.python.org/downloads/release/python-375/
-OS X: https://www.python.org/downloads/release/python-375/
+Create a file next to bot.py called ".env" and populate it with these contents
+
+```
+API_CLIENT_ID=<your TwitchAPI app's client ID from Twitch>
+USER_TOKEN=oauth:<your token>
+USER_CLIENT_ID=<your bot's client ID>
+BOT_NICK=<your bot's nickname>
+BOT_PREFIX=!
+CHANNEL=<your channel name>
+```
+
+Optionally, you can add support for listening to moderator activities by supplying the User IDs of the moderators whose actions in your channel you want to handle. If left empty or omitted then this pubsub action will not be registered.
+
+```
+MODERATORS=<moderator1_userid>,<moderator2_userid>,...
+```
+
+
+
+Note for coders: this file is in the .gitignore so that nobody accidentally pushes their secrets to the remote repository.
+
+## code
+
+1. Python 3.x(3.6+): https://www.python.org/downloads/ 
 1. Open a shell on your system: bash, iterm, cmd.exe
 1. run `pip install pipenv`
 1. change directory to where this project resides: cd ~/your/project/path, or even cd C:\your\project\path
 1. now execute `pipenv sync` to sync your local env with the dependencies of this project
+
+## run 
+
+```shell
+ > cd path\to\code\directory
+ path\to\code\directory\> pipenv run python bot.py
+```
+
+## documentation
+
+Pub Sub:
+1. twitch pubsub API docs: https://dev.twitch.tv/docs/pubsub#topics
+1. twitchio: https://twitchio.readthedocs.io/en/latest/exts/pubsub.html
+
+## troll bot
+
+if you would like to have a bot that replaces a word in your chatters' messages from time to time, like buttsbot, then add the following to your .env file
+
+```
+
+```
 
---
+++ bot.py
@@ -0,0 +1,124 @@
+import os # for importing env vars for the bot to use
+from twitchio.ext import commands, pubsub
+import twitchio
+import asyncio
+import time
+import threading
+
+client = twitchio.Client(token=os.environ['USER_TOKEN'])
+client.pubsub = pubsub.PubSubPool(client)
+
+@client.event()
+async def event_pubsub_bits(event: pubsub.PubSubBitsMessage):
+    pass # do stuff on bit redemptions
+
+@client.event()
+async def event_pubsub_channel_points(event: pubsub.PubSubChannelPointsMessage):
+    pass # do stuff on channel point redemptions
+
+@client.event()
+async def event_pubsub_bits_badge(event: pubsub.PubSubBitsBadgeMessage):
+    pass
+
+@client.event()
+async def event_pubsub_channel_points(event: pubsub.PubSubChannelPointsMessage):
+    pass
+
+@client.event()
+async def event_pubsub_moderation_user_action(event):
+    if(type(event) == pubsub.PubSubModerationActionBanRequest):
+        pass
+    elif(type(event) == pubsub.PubSubModerationActionChannelTerms):
+        pass
+    elif(type(event) == pubsub.PubSubModerationActionModeratorAdd):
+        pass
+    elif(type(event) == pubsub.PubSubModerationAction):
+        pass
+
+# The following two events are not yet supported in twitchio, but are mentioned in the docs
+@client.event()
+async def event_pubsub_channel_subscriptions(event):
+    pass
+
+@client.event()
+async def event_pubsub_whispers(event):
+    pass
+
+async def main():
+    topics = [
+        pubsub.channel_points(os.environ['USER_TOKEN'])[os.environ['CHANNEL_ID']],
+        pubsub.bits(os.environ['USER_TOKEN'])[os.environ['CHANNEL_ID']],
+        pubsub.bits_badge(os.environ['USER_TOKEN'])[os.environ['CHANNEL_ID']],
+        
+        # This support is not yet ready in twitchio. maybe soon?
+        # pubsub.channel_subscriptions(os.environ['USER_TOKEN'])[os.environ['CHANNEL_ID']],
+        # pubsub.whispers(os.environ['USER_TOKEN'])
+    ]
+
+    for mod_id in os.environ.get("MODERATORS", "").strip().split(","):
+        topics.append(pubsub.bits_badge(os.environ['USER_TOKEN'])[os.environ['CHANNEL_ID']][mod_id])
+    
+    await client.pubsub.subscribe_topics(topics)
+    await client.start()
+
+
+class PubSubListener(object):
+    def __init__(self, interval=1):
+        self.interval = interval
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
+
+    def run(self):
+        time.sleep(self.interval)
+        client.loop.run_until_complete(main())
+            
+
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        # prefix can be a callable, which returns a list of strings or a string...
+        # initial_channels can also be a callable which returns a list of strings...
+        super().__init__(token=os.environ['USER_TOKEN'],
+                         prefix=os.environ['BOT_PREFIX'],
+                         initial_channels=[os.environ['CHANNEL']],
+                         )
+
+    async def event_ready(self):
+        # Notify us when everything is ready!
+        # We are logged in and ready to chat and use commands...
+        print(f'Logged in as | {self.nick}')
+
+    async def event_message(self, message):
+        # Messages with echo set to True are messages sent by the bot...
+        # For now we just want to ignore them...
+        if message.echo:
+            return
+
+        if not str(message.content).startswith(os.environ['BOT_PREFIX']):            
+            # Print the contents of our message to console...
+            print(message.content)
+            pass
+
+        # Since we have commands and are overriding the default `event_message`
+        # We must let the bot know we want to handle and invoke our commands...
+        await self.handle_commands(message)
+    
+    @commands.command()
+    async def hello(self, ctx: commands.Context):
+        # Here we have a command hello, we can invoke our command with our prefix and command name
+        # e.g ?hello
+        # We can also give our commands aliases (different names) to invoke with.
+
+        # Send a hello back!
+        # Sending a reply back to the channel is easy... Below is an example.
+        await ctx.send(f'Hello {ctx.author.name}!')
+        
+        
+if __name__ == "__main__":
+    bot = Bot()
+    # pubsub_listener = PubSubListener()
+    bot.run() # the application will block on this call
+    print("bot shut down")
